aaa489ca85e7550012783e2fbf50d50e1685fdf7
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-05-02 11:44:14 -0700

    starting to port focus over to a standalone app

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,42 @@
+use dotenv::dotenv;
 use gpui::*;
 
-struct HelloWorld {
-    text: SharedString,
-}
+mod domain;
+mod elements;
+mod events;
+mod models;
+mod store;
+mod theme;
+mod views;
 
-actions!(counter, [Quit]);
+use domain::*;
+use events::*;
+use models::*;
+use views::*;
 
-impl Render for HelloWorld {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
-        div()
-            .flex()
-            .bg(rgb(0x2e7d32))
-            .size(Length::Definite(Pixels(300.0).into()))
-            .justify_center()
-            .items_center()
-            .shadow_lg()
-            .border()
-            .border_color(rgb(0x0000ff))
-            .text_xl()
-            .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
-    }
-}
+actions!(kallax, [Quit]);
 
 fn main() {
-    App::new().run(|cx: &mut AppContext| {
-        let bounds = Bounds::centered(None, size(px(600.0), px(600.0)), cx);
-        cx.open_window(
-            WindowOptions {
-                bounds: Some(bounds),
-                ..Default::default()
-            },
-            |cx| {
-                cx.new_view(|_cx| HelloWorld {
-                    text: "World".into(),
-                })
-            },
-        );
+    dotenv().ok();
+
+    App::new().run(|cx| {
         cx.activate(true);
         cx.on_action(|_: &Quit, cx| cx.quit());
         cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
-        cx.set_menus(vec![Menu {
-            name: "Image",
-            items: vec![MenuItem::action("Quit", Quit)],
-        }]);
+
+        let window_options = WindowOptions {
+            titlebar: Some(TitlebarOptions {
+                title: Some(SharedString::from("Kallax")),
+                appears_transparent: true,
+                ..Default::default()
+            }),
+            bounds: WindowBounds::Fixed(Bounds {
+                size: size(px(800.), px(600.)).into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        };
+
+        cx.open_window(window_options, |cx| cx.new_view(Kallax::new));
     });
 }
